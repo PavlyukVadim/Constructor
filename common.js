355e29888c0e7939762cac76ea2d9793ad9444d0
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2016-08-10 13:22:31 -0700

    adding text

--- common.js
+++ common.js
@@ -7,7 +7,9 @@ var filesElement, files,
     logos = [],
     downloadsElement,
     countPhotoElement,
-    countPhoto = 2;
+    countPhoto = 2,
+    addTextElement,
+    textWidget;
 
 filesElement = document.getElementById('files');
 body = document.getElementsByTagName('body')[0];
@@ -15,6 +17,8 @@ canvasesElements = document.getElementById("canvases");
 editCanvasElement = document.getElementById("edit-canvas");
 downloadsElement = document.getElementById("downloads");
 countPhotoElement = document.getElementById("count-photo");
+addTextElement = document.getElementById("add-text");
+textWidget = document.getElementsByClassName("widget")[0];
 
 window.onload = function(){
     
@@ -174,4 +178,122 @@ function downloadCanvas(i, link, filename) {
 
 countPhotoElement.onchange = function() {
     countPhoto = this.value;
+}
+
+$("#add-text").click(function() {   
+    $(".widget-text").toggle("slows");
+    $("#add-text").toggleClass("active");
+    if ($("#add-text").hasClass("active")) {
+        this.value = "Згорнути";
+        var text = new fabric.Text('Мой текст', {left: 10, top: 100});
+        fabricEditCanvas.add(text); 
+        fabricEditCanvas.setActiveObject(text);
+        currentTextElement = text;
+    }
+    if(!$("#add-text").hasClass("active")){
+       this.value = "Добавити текст";
+    }
+});
+
+var textArea = document.getElementById('input-text');
+textArea.oninput = function() {
+    currentTextElement = fabricEditCanvas.getActiveObject();
+    currentTextElement.text = textArea.value;
+    fabricEditCanvas.setActiveObject(currentTextElement); 
+}; 
+
+var textColorElement = document.getElementById('text-color-input');
+textColorElement.onchange = function() {
+    currentTextElement.setColor('#' + textColorElement.value);
+    fabricEditCanvas.setActiveObject(currentTextElement);
+}
+
+
+
+var widgetElement = document.getElementsByClassName('widgets')[0],
+    buttonShowDetails;
+widgets = [];
+widgets['text'] = document.getElementsByClassName('widget-text')[0];
+widgets['layers'] = document.getElementsByClassName('widget-layers')[0];
+widgets['images'] = document.getElementsByClassName('widget-image')[0];
+
+var fontsArr = ['Andika', 'Anonymous', 'Bad Script', 'Ubuntu'],
+    fontList = document.getElementsByClassName('font-list')[0];
+
+
+var fontControls = [];
+fontControls['bold'] = widgets['text'].getElementsByClassName('bold')[0];
+fontControls['italic'] = widgets['text'].getElementsByClassName('italic')[0];
+var underLineElement = widgets['text'].getElementsByClassName('underline')[0];
+var fontElement = widgets['text'].getElementsByClassName('font')[0];
+creatingFonts = false;
+
+fontElement.onclick = function() {
+    if (!creatingFonts) {
+        createFonts();
+    }
+    fontList.style.display = 'block';
+}
+
+for (var name in fontControls) {
+    fontControls[name].onclick = addFontAlignHandlers(fontControls[name], 'fontStyle', name, fontControls);
+}
+
+// Choose font or text-align
+function addFontAlignHandlers(element, type, style, arr) {
+    return function() {
+        fabricEditCanvas.setActiveObject(currentTextElement);
+        for (var name in arr) {
+            if(name != style) {
+                arr[name].classList.remove('active');   
+            }
+        }
+        if (element.classList.contains('active')) {
+            element.classList.remove('active');
+            currentTextElement[type] = '';
+        }
+        else {
+            element.classList.add('active');
+            currentTextElement[type] = style;
+        }
+        fabricEditCanvas.setActiveObject(currentTextElement);
+    }
+}
+underLineElement.onclick = function() {
+    if (underLineElement.classList.contains('active')) {
+        underLineElement.classList.remove('active');
+        currentTextElement['textDecoration'] = '';
+    }
+    else {
+        underLineElement.classList.add('active');
+        currentTextElement['textDecoration'] = 'underline';
+    }
+    fabricEditCanvas.setActiveObject(currentTextElement);
+};
+
+function createFonts(){
+    var fontList = document.getElementsByClassName('font-list')[0];
+    for (var index in fontsArr){
+        var li = document.createElement('li');
+        var div = document.createElement('div');
+        div.innerHTML = fontsArr[index];
+        li.style.fontFamily = fontsArr[index];
+        li.appendChild(div);
+        li.onclick = addHandlerFonts(li, fontsArr[index]);
+        fontList.appendChild(li);
+    }
+    creatingFonts = true;
+}
+
+function addHandlerFonts(element, font){
+    return function() {
+        var fontList = document.getElementsByClassName('font-list')[0];
+        var fonts = fontList.getElementsByTagName('li');
+        for (var i = 0; i < fonts.length; i++) {
+            fonts[i].classList.remove('active');
+        }
+        element.classList.add('active');
+        currentTextElement.fontFamily = font;
+        fabricEditCanvas.setActiveObject(currentTextElement);
+    }
 }
