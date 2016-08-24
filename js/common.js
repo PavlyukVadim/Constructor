f0c4b7a6672cc05230644234ad410fb13cc0d3d9
amadev <pavlyuk.dev@gmail.com>
2016-08-24 09:35:10 +0300

    structure of the landing

--- js/common.js
+++ js/common.js
@@ -3,4 +3,18 @@ $(document).ready(function ($) {
   $(".parallax-window").parallax();
   $(".loader_inner").delay(400).fadeOut();
   $(".loader").delay(600).fadeOut("slow");
+  $(".review-photo-1").css("height", $(".review").height());
+  
+  
+  $(".section-title").animated("fadeInDown", "fadeOutDown");
+  $(".section-explanation").animated("fadeInUp", "fadeOutDown");
+  $(".right-side-review").animated("fadeInRight", "fadeOutDown");
+  $(".left-side-review").animated("fadeInLeft", "fadeOutDown");
+  $(".img_1, .img_2, .img_3, .thumbnail img").animated("flipInY", "fadeOutDown");
+  
+  $("a[rel='m_PageScroll2id']").mPageScroll2id({/*offset:50*/});
+  
+});
+$(window).resize(function () {
+  $(".review-photo-1").css("height", $(".review").height());
 });
